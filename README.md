42fe395c0e6600315c04dda66dcb9e46d163a88c
FelipeGrellmannSouza <felipegrellmann1@gmail.com>
2025-01-28 08:09:45 -0300

    Configuração e criação do model e das rotas

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # to-do-api
+
+Este projeto utilizando nodeJS serve para por em praticas conceitos de criação de uma API, o projeto consiste em uma API de lista de tarefas onde será uma tarefa podera ser Criada, listada, editada e deletada. 
---
+++ src/controllers/todoController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { Todo } from '../models/Todo';
+
+export const all = async (req: Request, res: Response) => {
+
+}
+
+export const add = async () => {
+
+}
+
+export const update = async () => {
+
+}
+
+export const remove = async () => {
+
+}
