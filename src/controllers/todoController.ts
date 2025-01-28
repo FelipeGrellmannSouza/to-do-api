708681c1c70699669b032e3940afa4ee20788ee2
FelipeGrellmannSouza <felipegrellmann1@gmail.com>
2025-01-28 09:47:05 -0300

    função list

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -2,7 +2,8 @@ import { Request, Response } from 'express';
 import { Todo } from '../models/Todo';
 
 export const all = async (req: Request, res: Response) => {
-
+    const list = await Todo.findAll();
+    res.json({ list });
 }
 
 export const add = async () => {
